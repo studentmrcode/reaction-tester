9c9681cd548d142a711360a7edcd0097cef1454f
David <davidk@mrcodeswildride.com>
2018-12-05 14:29:05 +0000

    init

---
+++ script.js
@@ -0,0 +1,11 @@
+// boolean indicating whether the user has started the game or not
+var gameStarted = false;
+
+// boolean indicating whether the page has changed color or not
+var colorChanged = false;
+
+// variable to store the timer ID for changing the page color
+var timerId = null;
+
+// variable to store the time when the page color changed
+var colorChangeTime = null;
