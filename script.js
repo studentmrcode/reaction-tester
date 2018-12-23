a1e62d6f1f91c3e2383cf2e4e9acd82af424ad74
David <davidk@mrcodeswildride.com>
2018-12-23 18:57:14 +0000

    changed timerId to timeoutId

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ var gameStarted = false;
 // boolean indicating whether the page has changed color or not
 var colorChanged = false;
 
-// variable to store the timer ID for changing the page color
-var timerId = null;
+// variable to store the timeout ID for changing the page color
+var timeoutId = null;
 
 // variable to store the time when the page color changed
 var colorChangeTime = null;
